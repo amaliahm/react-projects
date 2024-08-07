554d7027e28babc0582d71d16d244b8a6736b613
amina <127314669+amaliahm@users.noreply.github.com>
2024-08-07 13:22:56 +0100

    update app

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,11 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+import data from './data'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+
+  const [tours, setTours] = useState(data)
+  console.log(tours)
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  if (tours.length === 0) {
+    return <main>
+      <div className='title'>
+        <h2>no tours left</h2>
+        <button onClick={() => setTours(data)} className='btn'>refresh</button>
+      </div>
+    </main>
+  }
+
+  return <main>
+    <Tours tours={tours} removeTour={removeTour} />
+  </main>
 }
 
 export default App
